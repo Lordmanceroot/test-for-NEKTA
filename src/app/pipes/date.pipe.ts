14edec0ecc732adf14e3b2e7df24b125b7f0e008
Vladimir <lordmancer1998electro@gmail.com>
2022-09-07 00:46:03 +0400

    minor fixes

--- src/app/pipes/date.pipe.ts
+++ src/app/pipes/date.pipe.ts
@@ -7,6 +7,6 @@ export class DatePipe implements PipeTransform {
   transform(date: string): string {
     const currentDate = new Date(+date * 1000).toLocaleDateString()
     const currentTime = new Date(+date * 1000).toLocaleTimeString()
-    return `${currentDate} ${currentTime}`
+    return ` Дата: ${currentDate} | Время: ${currentTime}`
   }
 }
